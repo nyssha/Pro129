730852d70ec6f3974d26399737db1945e5e82675
nyssha <65846349+nyssha@users.noreply.github.com>
2023-04-29 14:41:09 +0530

    Add files via upload

---
+++ data_pre-proccesing.py
@@ -0,0 +1,29 @@
+import csv
+dataset_1=[]
+dataset_2=[]
+
+with open("final.csv","r") as  f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        dataset_1.append(row)
+
+with open("archive_dataset_sorted1.csv","r") as  f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        dataset_2.append(row)        
+
+headers_1=dataset_1[0]
+star_data_1=dataset_1[1:]
+
+headers_2=dataset_2[0]
+star_data_2=dataset_2[1:]
+
+headers=headers_1+headers_2
+star_data=[]
+for index,data_row in enumerate(star_data_1):
+    star_data.append(star_data_1[index]+star_data_2[index]) 
+
+with open("merged_dataset.csv","a+") as f:
+    csvwriter=csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(star_data)    
